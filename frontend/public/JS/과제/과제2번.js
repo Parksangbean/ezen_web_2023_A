30cb19aace90a014ce88d01eb392e537a5996023
504 <504@DESKTOP-G2B2I5O>
2023-06-22 13:11:04 +0900

    2032-06-22

--- frontend/public/JS/과제/과제2번.js
+++ frontend/public/JS/과제/과제2번.js
@@ -9,8 +9,32 @@ function join(){
 			 비밀번호.push(password.value);
 			 alert('회원가입 완료')				
 		} else {
-			alert('로그인 실패')			
+			alert('회원가입 실패')			
 		}	
 		id.value = ""
-			password.value = ""		
+		password.value = ""		
+}
+function login(){
+	let id = document.querySelector(".id")
+	let password = document.querySelector(".password")
+				
+		if(login.value.length>=8 && password.value.length>=8 && id.indexOf(login.value)!=-1 && password.indexOf(password.value)!=-1){
+		 아이디.push(id.value);
+		 비밀번호.push(password.value);
+		alert('로그인이 완료되었습니다')
+	}else if(login.value.length<8){
+		아이디.push(id.value);
+		비밀번호.push(password.value);
+		alert('아이디를 8글자 이상 입력하세요')
+	}
+	else if(password.value.length<8){
+		아이디.push(id.value);
+		비밀번호.push(password.value);
+		alert('비밀번호를 8글자 이상 입력하세요')
+	}else{
+		
+		alert('아이디 또는 비밀번호를 확인해주세요')
+	}
+		id.value = "" 
+		password.value = ""		
 }
