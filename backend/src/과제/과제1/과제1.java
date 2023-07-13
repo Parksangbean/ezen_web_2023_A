b3ca46efa9f0e5d9b613cdb7bc008f5135bb0a81
504 <504@DESKTOP-CM43SMF>
2023-07-13 18:00:56 +0900

    day26 18:00

---
+++ backend/src/과제/과제1/과제1.java
@@ -0,0 +1,24 @@
+package 과제.과제1;
+
+public class 과제1 {
+	public static void main(String[] args) {
+		/*
+		 1. 4번
+		 2.2번.3번.6번,7번
+		 3. 정수타입 : 1byte(byte) 2byte(short,char) 4byte(int,float) 8byte(long)
+		 	실수타입 : 4byte(float) 8byte(double)
+		 	논리타입 : boolean
+		 4.타입 : int double
+		 	변수 이름 : age, price
+		 	리터럴 : 10 , 3.14
+		 5.3번
+		 6.3번
+		 7.6번
+		 8.4번
+		 9.3번
+		 10.4번
+		 11. 13,16째 줄
+ 
+		 */
+	}
+}
