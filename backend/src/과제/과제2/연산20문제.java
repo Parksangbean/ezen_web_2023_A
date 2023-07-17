a383f283c152867298191a191dc9a5cdc33f9dcb
504 <504@DESKTOP-CM43SMF>
2023-07-17 12:19:38 +0900

    12:19

--- backend/src/과제/과제2/연산20문제.java
+++ backend/src/과제/과제2/연산20문제.java
@@ -7,19 +7,23 @@ public class 연산20문제 {
 	public static void main(String[] args) {
 		
 		//------ 문제1 여기에 풀이-------//
-		//System.out.println("|\\_/|");
-		//System.out.println("|q p |  /}");
-		//System.out.println("( 0 \\");
-		
+				System.out.println("문제1 : 강아지 console 출력");
+				System.out.println("|\\_/|");
+				System.out.println("|q p|   /}");
+				System.out.println("( 0 )\"\"\"\\");
+				System.out.println("|\"^\"`    |");
+				System.out.println("||_/=\\\\__|");
 		//------ 문제2 여기에 풀이-------//
-	/*	Scanner scanner = new Scanner(System.in);
+		
+		Scanner scanner = new Scanner(System.in);
 		String 작성자 =scanner.next(); 
 		String 내용 =scanner.next();
 		String 날짜 =scanner.next();
 		System.out.println("-------------방문록--------");
 		System.out.println("| 순번|작성자|내용|날짜|");
 		System.out.println("1"+"|"+작성자+"|"+내용+"|"+날짜+"|");
-		System.out.println("-----------------------------");		*/
+		System.out.println("-----------------------------");		
+		
 		//------ 문제3 여기에 풀이-------//
 		/*[입력조건]
 				기본급[정수] , 수당[정수]
@@ -27,10 +31,10 @@ public class 연산20문제 {
 				 실수령액 = 기본급 + 수당 - 세금[기본급10%] 
 			[출력조건] 
 				실수령액 출력[ 소수점 없이  ] */
-		/*int 기본급=scanner.nextInt(); 
+		int 기본급=scanner.nextInt(); 
 		int 수당=scanner.nextInt();
 		int 실수령액=기본급+수당-(기본급/10);
-		System.out.println("실수령액="+실수령액);*/
+		System.out.println("실수령액="+실수령액);
 		
 		//------ 문제4 여기에 풀이-------//
 		 /*[입력조건 ] 
@@ -43,8 +47,8 @@ public class 연산20문제 {
 					 만원 5장
 					 천원 6장 
 					 백원 7개*/
-		Scanner scanner = new Scanner(System.in);
-		/*
+		//Scanner scanner = new Scanner(System.in);
+		
 		int 금액 = scanner.nextInt(); 
 		int 십만원=금액/100000;
 		int 만원 = ( (금액-(십만원*100000)) / 10000);
@@ -54,44 +58,45 @@ public class 연산20문제 {
 		System.out.println("만원: "+만원+"장");
 		System.out.println("천원: "+천원+"장");
 		System.out.println("백원: "+백원+"장");
-		*/
+		
 		
 		//------ 문제5 여기에 풀이-------//
 		//하나의 정수를 입력받아 7의 배수인지 출력[ 'O' 혹은 'X' 로 출력]
-		/*
+		
 		int a =  scanner.nextInt(); 
-		System.err.println(a%7==0 ? 'o':'x');*/
+		System.err.println(a%7==0 ? 'o':'x');
 		//------ 문제6 여기에 풀이-------//
 		// 하나의 정수를 입력받아 홀수 인지 출력 [ 'O' 혹은 'X' 로 출력]
-		/*int b =  scanner.nextInt(); 
-		System.err.println(b%2==1 ? 'o':'x');*/
+		int b =  scanner.nextInt(); 
+		System.err.println(b%2==1 ? 'o':'x');
 		
 		//------ 문제7 여기에 풀이-------//
 		// 하나의 정수를 입력받아 7의 배수 이면서 짝수 인지 출력 [ 'O' 혹은 'X' 로 출력 ]
-		/*
+		
 		int c =  scanner.nextInt(); 
 		System.out.println(c%7==0&&c%2==0?'0':'x');
-		*/
+		
 		//------ 문제8 여기에 풀이-------//
 		//하나의 정수를 입력받아 7의 배수 이거나 홀수 인지 출력 [ 'O' 혹은 'X' 로 출력 ]
-		/*
+		
 		int d =  scanner.nextInt(); 
 		System.out.println(d%7==0 && d%2==1? '0' : 'x');
-		*/
+		
 		//------ 문제9 여기에 풀이-------//
 		// 두개의 정수를 입력받아 더 큰수 출력
-		/*
+		
 		int 정수=  scanner.nextInt(); 
 		int 정수1 =  scanner.nextInt(); 
 		System.out.println(정수>정수1?정수:정수1);
-		*/
+		
 		//------ 문제10 여기에 풀이-------//
 		// 반지름을 입력받아 원 넓이 출력하기 
 		//	계산식) 원 넓이 공식 => 반지름 * 반지름 * 원주율[3.14]
-		/*
-		int 반지름 = scanner.nextInt(); 
-		int 넓이 =반지름*반지름*(3.14);
-		System.out.println("원의넓이는: "+넓이); */
+		
+		System.out.println("문제10 : 반지름을 입력받아 원 넓이 출력하기");
+		System.out.print("반지름을 입력하세요: ");
+		int 반지름 = scanner.nextInt();
+		System.out.println((반지름*반지름*3.14));
 		//------ 문제11 여기에 풀이-------//
 		// 두 실수를 입력받아 앞실수의 값이 뒤의 값의 몇% 인지 출력하기
 		// 예) 54.5   84.3 실수 2개입력시 결과는   64.285714%
@@ -105,7 +110,7 @@ public class 연산20문제 {
 		//------ 문제12 여기에 풀이-------//
 		// 문제12 : 사다리꼴 넓이 구하기[윗변과 밑변 높이를 입력받아 출력하기 
 		//계산식) 사다리꼴 계산식 = > (윗변 + 밑변) * 높이 / 2
-		/*
+		
 		System.out.print("윗변을 입력 해주세요: ");
 		int 윗변 =scanner.nextInt();
 		System.out.print("밑변을 입력 해주세요: ");
@@ -113,15 +118,15 @@ public class 연산20문제 {
 		System.out.print("높이를 입력 해주세요: ");
 		int 높이 =scanner.nextInt();
 		System.out.println("사다리꼴 넓이 : " +(윗변+밑변)*높이/2);
-		*/
+		
 		//------ 문제13 여기에 풀이-------//
 		// 키를 정수를 입력받아 표준체중 출력하기 
 		//계산식) 표준체중 계산식 = > (키 - 100) * 0.9
-		/*
+		
 		System.out.print("키를 입력해주세요: ");
 		int key = scanner.nextInt();	
 		System.out.println("표준체중: "+(key-100)*0.9);
-		*/
+		
 		//------ 문제14 여기에 풀이-------//
 		//문제14: 키와 몸무게를 입력받아 BMI 출력하기 
 		//계산식) BMI 계산식 = > 몸무게 / ((키 / 100) * (키 / 100))
@@ -135,9 +140,9 @@ public class 연산20문제 {
 		//------ 문제15 여기에 풀이-------//
 		//문제15: inch 를 입력받아 cm 로 변환하기 
 		//계산식) 1 inch -> 2.54cm
-		/*
+		
 		int inch = scanner.nextInt();
-		System.out.println("1 inch: "+(inch*2.54)+"cm");*/
+		System.out.println("1 inch: "+(inch*2.54)+"cm");
 		//------ 문제16 여기에 풀이-------//
 		//문제16:  중간고사, 기말고사, 수행평가를 입력받아 반영비율별 계산하여 소수 둘째자리까지 점수 출력하시오 
 		//계산식 반영비율)  중간고사 반영비율 => 30 %  / 기말고사 반영비율 => 30 %   / 수행평가 반영비율 => 40 %
@@ -154,21 +159,17 @@ public class 연산20문제 {
 		*/
 		//------ 문제17 여기에 풀이-------//
 		//문제17 :  연산 순서 나열 하고 printf() 에 출력되는 x 와 y 값을 예측하시오.
-		//int x = 10;
-		//int y = x-- + 5 + --x;
-		//printf(" x의 값 : %d , y의값 :  %d ", x, y)
-		// x=8	
-		//	y=23
+		// x=8 , y=23
 		
 		//------ 문제18 여기에 풀이-------//
 		//  나이를 입력받아 나이가 10세이상이면 학생 , 20세이상이면 성인 , 40세이상이면 중년 으로 출력하기
-		/*
+		
 		int age = scanner.nextInt();
 		System.out.println(age>=40?"중년":age>=20?"성인":"학생");
-		*/
+		
 		//------ 문제19 여기에 풀이-------//
 		// 국어 , 영어 , 수학 점수를 입력받아 각 변수에 저장하고 총점(소수점 0자리) 출력 , 평균(소수점 2자리 까지) 출력
-		/*
+		
 		System.out.print("국어 점수를 입력해주세요: ");
 		int korean =scanner.nextInt();
 		System.out.print("영어 점수를 입력해주세요: ");
@@ -178,10 +179,11 @@ public class 연산20문제 {
 		
 		double average = (korean + en + math)/3;
 		System.out.printf("총점:%d\n", korean+en+math);
-		System.out.printf("평균:%.2f", average);*/
+		System.out.printf("평균:%.2f", average);
 		
 		//------ 문제20 여기에 풀이-------//5
 		// 문제20 : 아이디[문자열] 와 비밀번호[문자열] 를 입력받아 아이디가 'admin' 이고 비밀번호가 '1234' 와 일치하면 로그인성공 아니면 로그인실패 출력
+		
 		System.out.print("아이디를 입력해주세요: ");
 		String id = scanner.nextLine();
 		System.out.print("비밀번호를 입력해주세요: ");
@@ -189,7 +191,9 @@ public class 연산20문제 {
 		
 		System.out.println(id.equals("admin") && pwd.equals("1234") ? "로그인성공" : "로그인실패");
 		
+		
 		//------ 문제21 여기에 풀이-------//
+		/*
 		System.out.print("정수를 입력해주세요: ");
 		int a = scanner.nextInt();
 		System.out.print("정수를 입력해주세요: ");
@@ -197,7 +201,7 @@ public class 연산20문제 {
 		System.out.print("정수를 입력해주세요: ");
 		int c = scanner.nextInt();
 		System.out.println(a>b && a>c ? a : b>c ? b : c);
-		
+		*/
 		
 	}
 
