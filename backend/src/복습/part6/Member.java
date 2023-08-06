83963be958c91982849a94908ccd385e243167be
hyuns <hyuns@SA>
2023-08-06 14:36:29 +0900

    1212

--- backend/src/복습/part6/Member.java
+++ backend/src/복습/part6/Member.java
@@ -8,9 +8,7 @@ public class Member {
 	String name;
 	String phone;
 	int age;
-	// 기본생성자
 	public Member() {}
-	// 풀생성자
 	public Member(String id, String pw, String name, String phone, int age) {
 		super();
 		this.id = id;
@@ -19,5 +17,6 @@ public class Member {
 		this.phone = phone;
 		this.age = age;
 	}
+	
 	
 }
--- backend/src/복습/part6/MemberSys.java
+++ backend/src/복습/part6/MemberSys.java
@@ -8,58 +8,62 @@ public class MemberSys {
 		Scanner sc = new Scanner(System.in);
 		
 		while(true) {
-			System.out.println("------------- 회원 시스템 -----------");
-			System.out.println("1.회원가입 2.로그인 3.아이디찾기 4.비밀번호찾기        선택>>"); int ch = sc.nextInt();
+			System.out.println("---------- 회원 시스템 -----------");
+			System.out.println("1.회원가입 2.로그인 3.아이디 찾기 4. 비밀번호 찾기     선택>>"); int ch = sc.nextInt();
 			
 			if(ch==1) {
-				System.out.println("--------- 회원가입 --------");
+				System.out.println("-------- 회원가입 ----------");
 				System.out.println("아이디: "); String id = sc.next();
 				System.out.println("비밀번호: "); String pw = sc.next();
 				System.out.println("이름: "); String name = sc.next();
 				System.out.println("전화번호: "); String phone = sc.next();
-				System.out.println("나이: "); int age=sc.nextInt();
+				System.out.println("나이: "); int age =sc.nextInt();
+				Member Member= new Member(id,pw,name,phone,age);
 				
-				Member member = new Member( id,  pw,  name, phone , age);
 				for(int i=0; i<MemberList.length; i++) {
 					if(MemberList[i]==null) {
-						MemberList[i]=member;
+						MemberList[i]=Member;
 					}
 				}
 				System.out.println("안내) 회원가입 성공");
 			}
-			else if (ch==2) {
-				System.out.println("-------- 로그인 -------");
+			else if(ch==2) {
+				System.out.println("-------- 로그인 ----------");
 				System.out.println("아이디: "); String id = sc.next();
 				System.out.println("비밀번호: "); String pw = sc.next();
-				int login=-1;
-				for(int i = 0; i<MemberList.length; i++) {
+				int login = -1;
+				for(int i =0; i<MemberList.length; i++) {
 					if(MemberList[i]!=null&&MemberList[i].pw.equals(pw)) {
-						login=i;
+						login =i;
 					}
-				}
-				if(login>=0) {
+				}if(login>=0) {
 					System.out.println("안내) 로그인 성공");
 				}else {
-					System.out.println("안내) 로그인 실패 : 아이디와 비밀번호가 일치하지 않습니다.");
+					System.out.println("안내) 로그인 실패");
 				}
 			}
-			else if (ch==3) {
-				System.out.println("--------- 아이디 찾기 ----------");
-				System.out.println("이름: "); String name = sc.next();
-				System.out.println("전화번호: "); String phone = sc.next();
-				for(int i=0; i<MemberList.length; i++) {
-					if(MemberList[i]!=null&&MemberList[i].phone.equals(phone)) {
-						System.out.println("안내)회원님의 아이디 : "+MemberList[i].id+"입니다.");
-						break;
-					}
-				}
-			}
-			else if (ch==4) {
+			else if(ch==3) {
+				System.out.println("---------- 아이디 찾기 ----------");
 				System.out.println("이름: "); String name = sc.next();
 				System.out.println("전화번호: "); String phone = sc.next();
 				for(int i =0; i<MemberList.length; i++) {
 					if(MemberList[i]!=null&&MemberList[i].name.equals(name)) {
-						System.out.println("안내) 회원님의 비밀번호 :"+MemberList[i].pw+"입니다.");
+						System.out.println("안내) 회원님의 아이디:"+MemberList[i].id);
+						break;
+					}else {
+						System.out.println("안내) 이름 혹은 전화번호가 일치하지 않습니다.");
+						break;
+					}
+				}
+				
+			}
+			else if(ch==4) {
+				System.out.println("---------- 비밀번호 찾기 ----------");
+				System.out.println("이름: "); String name = sc.next();
+				System.out.println("전화번호: "); String phone = sc.next();
+				for(int i =0; i<MemberList.length; i++) {
+					if(MemberList[i]!=null&&MemberList[i].phone.equals(phone)) {
+						System.out.println("안내) 회원님의 비밀번호:"+MemberList[i].pw);
 						break;
 					}
 				}
