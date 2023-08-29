d7559ee5685906fb204b27683f281307299995b6
hyuns <hyuns@SA>
2023-08-30 03:56:56 +0900

    2023 08-30 3:56 커밋

--- jspweb/src/main/java/model/dao/MemberDao.java
+++ jspweb/src/main/java/model/dao/MemberDao.java
@@ -11,46 +11,44 @@ public class MemberDao extends Dao{
 	
 	
 	// 1. 회원가입
-	public boolean signup(MemberDto dto) {
-		try {
-			String sql="insert into member(mid , mpwd, memail , mimg) value(?,?,?,?)";
-			ps=conn.prepareStatement(sql);
-			ps.setString(1, dto.getMid());
-			ps.setString(2, dto.getMpwd());
-			ps.setString(3, dto.getMemail());
-			ps.setString(4, dto.getMimg());
-			int row=ps.executeUpdate();
-			if(row==1)return true;
-			
-		}catch (Exception e) {System.out.println(e);}
+		public boolean signup( MemberDto dto ) {
+			try {
+				String sql = "insert into member( mid , mpwd , memail , mimg ) values( ? , ? , ? , ? )";
+				ps = conn.prepareStatement(sql);
+				ps.setString( 1 , dto.getMid() );
+				ps.setString( 2 , dto.getMpwd() );
+				ps.setString( 3 , dto.getMemail() );
+				ps.setString( 4 , dto.getMimg() );
+				int row = ps.executeUpdate();
+				if( row == 1 )return true;
+			}catch (Exception e) {System.out.println(e);}
+			return false;
+		}
+		// 2. 로그인 
 		
-		return false;
-	}
-	
-	
-	// 2. 로그인
-	
-	// 3. 아이디찾기
-	
-	// 4. 비밀번호 찾기
-	
-	// 5. 내정보 호출
-	
-	// 6. 아이디/이메일 중복검사 [ 인수 : 검사할아이디 / 반환 true(중복있어) , false(중복없어)
-	public boolean findIdorEmail(String type,String mid) {
-		try {
-			String sql ="select * from member where "+type+" =?";
-			ps = conn.prepareStatement(sql);
-			ps.setString(1, data);
-			rs=ps.executeQuery();
-			// [while : 결과 레코드 여러개 검사 vs if : 결과 레코드 한개 검사]
-			if(rs.next()) return true;
+		// 3. 아이디찾기
 		
-		}catch (Exception e) {System.out.println(e);}
-		return false;
+		// 4. 비밀번호찾기 
+		
+		// 5. 내정보 호출 
+		
+		// 6. 아이디/이메일 중복검사 [ 인수 : 검사할아이디 / 반환 : true(중복있어) , false(중복없어) 
+			// - type(mid,memail) : 필드명 / data(입력받은mid,memail) : 필드에서 찾을 값 
+		public boolean findIdOrEmail( String type , String data ) {
+			try {
+				String sql = "select * from member where "+type+" = ?";
+				ps = conn.prepareStatement(sql);
+				ps.setString( 1 , data );
+				rs = ps.executeQuery();
+				// [ while : 결과 레코드 여러개 검사 vs if : 결과 레코드 한개 검사 ]
+				if( rs.next() ) return true;
+			}catch (Exception e) {System.out.println(e);}
+			return false;
+		}
+		// 7. 회원수정
+		
+		// 8. 회원탈퇴
+		
+		
+		
 	}
-	
-	// 7. 회원수정
-	
-	// 8. 회원탈퇴
-}
